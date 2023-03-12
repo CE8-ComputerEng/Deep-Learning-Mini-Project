b3b6163cb5fd0c3486673d2725e04ce6e1986f3f
tallosim <tallosi.mate@simonyi.bme.hu>
2023-03-12 17:00:08 +0100

    Update model defination.

--- Model.py
+++ Model.py
@@ -15,7 +15,7 @@ class CoughClassifier(nn.Module):
         self.bn1 = nn.BatchNorm2d(8)
         nn.init.kaiming_normal_(self.conv1.weight, a=0.1)
         self.conv1.bias.data.zero_()
-        conv_layers += [self.conv1, self.relu1, self.bn1]
+        # conv_layers += [self.conv1, self.relu1, self.bn1]
 
         # Second Convolution Block
         self.conv2 = nn.Conv2d(8, 16, kernel_size=(3, 3), stride=(2, 2), padding=(1, 1))
@@ -23,7 +23,7 @@ class CoughClassifier(nn.Module):
         self.bn2 = nn.BatchNorm2d(16)
         nn.init.kaiming_normal_(self.conv2.weight, a=0.1)
         self.conv2.bias.data.zero_()
-        conv_layers += [self.conv2, self.relu2, self.bn2]
+        # conv_layers += [self.conv2, self.relu2, self.bn2]
 
         # Third Convolution Block
         self.conv3 = nn.Conv2d(16, 32, kernel_size=(3, 3), stride=(2, 2), padding=(1, 1))
@@ -31,7 +31,7 @@ class CoughClassifier(nn.Module):
         self.bn3 = nn.BatchNorm2d(32)
         nn.init.kaiming_normal_(self.conv3.weight, a=0.1)
         self.conv3.bias.data.zero_()
-        conv_layers += [self.conv3, self.relu3, self.bn3]
+        # conv_layers += [self.conv3, self.relu3, self.bn3]
 
         # Forth Convolution Block
         self.conv4 = nn.Conv2d(32, 64, kernel_size=(3, 3), stride=(2, 2), padding=(1, 1))
@@ -39,7 +39,7 @@ class CoughClassifier(nn.Module):
         self.bn4 = nn.BatchNorm2d(64)
         nn.init.kaiming_normal_(self.conv4.weight, a=0.1)
         self.conv4.bias.data.zero_()
-        conv_layers += [self.conv4, self.relu4, self.bn4]
+        # conv_layers += [self.conv4, self.relu4, self.bn4]
 
         # Linear Classifier
         self.ap = nn.AdaptiveAvgPool2d(output_size=1)
@@ -47,10 +47,24 @@ class CoughClassifier(nn.Module):
         self.act = nn.Softmax(dim=1)
 
         # Wrap the Convolutional Blocks
-        self.conv = nn.Sequential(*conv_layers)
+        # self.conv = nn.Sequential(*conv_layers)
         
     def forward(self, x):
-        x = self.conv(x)
+        x = self.conv1(x)
+        x = self.relu1(x)
+        x = self.bn1(x)
+        
+        x = self.conv2(x)
+        x = self.relu2(x)
+        x = self.bn2(x)
+        
+        x = self.conv3(x)
+        x = self.relu3(x)
+        x = self.bn3(x)
+        
+        x = self.conv4(x)
+        x = self.relu4(x)
+        x = self.bn4(x)
         
         x = self.ap(x)
         x = x.view(x.shape[0], -1)
