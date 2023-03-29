b98f2b32923adcb6364ffd844763753e8694b539
tallosim <tallosi.mate@simonyi.bme.hu>
2023-03-29 20:15:43 +0200

    Add data augmentation.

--- dataset.py
+++ dataset.py
@@ -77,11 +77,48 @@ class AudioUtils():
     
     
     @staticmethod
-    def get_spectrogram(signal, sample_rate, n_mels=64, n_fft=1024, top_db=80):
-        spectrogram = torchaudio.transforms.MelSpectrogram(sample_rate=sample_rate, n_mels=n_mels, n_fft=n_fft)(signal)
-        spectrogram = torchaudio.transforms.AmplitudeToDB(top_db=top_db)(spectrogram)
+    def get_spectrogram(signal, sample_rate, type='mel-spectrogram', n_mels=64, n_fft=1024, top_db=80):
+        if type == 'mel-spectrogram':
+            spectrogram = torchaudio.transforms.MelSpectrogram(sample_rate=sample_rate, n_mels=n_mels, n_fft=n_fft)(signal)
+            spectrogram = torchaudio.transforms.AmplitudeToDB(top_db=top_db)(spectrogram)
         
+        elif type == 'power-spectrogram':
+            spectrogram = torchaudio.transforms.Spectrogram(power=2.0, n_fft=n_fft)(signal)
+            spectrogram = torchaudio.transforms.AmplitudeToDB(top_db=top_db)(spectrogram)
+            
+        else:
+            raise Exception('Invalid spectrogram type: {}'.format(type))
+            
         return spectrogram
+    
+    
+    @staticmethod
+    def spectrogram_augment(spectrogram, masking_val:str|int|float='min', n_freq_masks=1, n_time_masks=1, max_mask_pct=0.1):
+        _, n_mels, n_frames = spectrogram.shape
+        aug_spectrogram = spectrogram.clone()
+        
+        if masking_val == 'min':
+            masking_val = aug_spectrogram.min()
+        elif masking_val == 'max':
+            masking_val = aug_spectrogram.max()
+        elif masking_val == 'mean':
+            masking_val = aug_spectrogram.mean()
+        elif masking_val == 'median':
+            masking_val = aug_spectrogram.median()
+        elif masking_val == 'random':
+            masking_val = random.random() * (aug_spectrogram.max() - aug_spectrogram.min()) + aug_spectrogram.min()
+        elif type(masking_val) == int or type(masking_val) == float:
+            masking_val = torch.tensor(masking_val)
+        
+        freq_mask_param = int(n_mels * max_mask_pct)
+        for _ in range(n_freq_masks):
+            aug_spectrogram = torchaudio.transforms.FrequencyMasking(freq_mask_param)(aug_spectrogram, masking_val)
+        
+        time_mask_param = int(n_frames * max_mask_pct)  
+        for _ in range(n_time_masks):
+            aug_spectrogram = torchaudio.transforms.TimeMasking(time_mask_param)(aug_spectrogram, masking_val)
+            
+        return aug_spectrogram  
 
 
 class CoughDataset(Dataset):
@@ -91,9 +128,14 @@ class CoughDataset(Dataset):
                  duration=10.0,
                  sample_rate=48000,
                  channels=1,
+                 spectrogram_type='mel-spectrogram',
                  n_mels=64,
                  n_fft=1024, 
-                 top_db=80):
+                 top_db=80,
+                 augment_masking_val='min',
+                 n_freq_masks=2,
+                 n_time_masks=1,
+                 max_mask_pct=0.1,):
         super().__init__()
         
         self.df = df
@@ -103,12 +145,20 @@ class CoughDataset(Dataset):
         self.sample_rate = sample_rate
         self.channels = channels
         
+        self.spectrogram_type = spectrogram_type
         self.n_mels = n_mels
         self.n_fft = n_fft
         self.top_db = top_db
         
+        self.augment_masking_val = augment_masking_val
+        self.n_freq_masks = n_freq_masks
+        self.n_time_masks = n_time_masks
+        self.max_mask_pct = max_mask_pct
+        
         self.label_encoder = preprocessing.LabelEncoder()
         self.label_encoder.fit(self.df['status'])
+        
+        self.class_counts = self.df['status'].value_counts().to_dict()
         
     def __len__(self) -> int:
         return len(self.df)
@@ -124,7 +174,7 @@ class CoughDataset(Dataset):
         signal = AudioUtils.resize(signal, self.duration, self.sample_rate)
 
         spectrogram = AudioUtils.get_spectrogram(signal, self.sample_rate, self.n_mels, self.n_fft, self.top_db)
-        # TODO: Add augmentation
+        spectrogram = AudioUtils.spectrogram_augment(spectrogram)
         
         label_id = self.label_encoder.transform([row['status']])[0]
         
